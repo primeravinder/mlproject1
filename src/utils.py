9d8c4947ac838093090ebb1df54870db9b74cc41
primeravinder <primeravinder@gmail.com>
2025-06-14 22:17:09 +0530

    prediction pipeline setup complete using flask: /predictdata - url

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from src.logger import logging
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path, obj):
     try:
@@ -44,3 +45,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
