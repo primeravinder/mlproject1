99d7efd3053dc2032508af8fe62de80122775fee
primeravinder <primeravinder@gmail.com>
2025-06-14 20:44:50 +0530

    Hyperparameter tuning integrated into model_training part in data_ingestion file.

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -38,17 +38,17 @@ class DataTransformation:
                 ]
             )
 
-            # logging.info("Numercial Features Scaling completed successfully.")
+            logging.info("Numercial Features Scaling completed successfully.")
 
             categorical_pipeline = Pipeline(
                 steps=[
-                    ("imputer", SimpleImputer(strategy="mode")),
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder", OneHotEncoder()),
-                    ("scaler", StandardScaler())
+                    ("scaler", StandardScaler(with_mean=False))  # OneHotEncoder returns sparse matrix, so we set with_mean=False
                 ]
             )
 
-            # logging.info("Categorical encoding completed successfully.")
+            logging.info("Categorical encoding completed successfully.")
 
             preprocessor = ColumnTransformer(
                 [
@@ -74,15 +74,39 @@ class DataTransformation:
             logging.info("obtaining preprocesssor object.")
 
             preprocessing_obj = self.get_data_transformer_obj()
-            target_col_name = "math_score"
+            target_column_name = "math_score"
             numerical_cols = ['writing_score', 'reading_score']
             # categorical_cols = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
 
-            input_features_train_df = train_df.drop(columns=[target_col_name], axis=1)
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
 
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
             
         except Exception as e:
---
+++ src/components/model_trainer.py
@@ -0,0 +1,115 @@
+import os
+import sys
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor,
+                              RandomForestRegressor)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import evaluate_models, save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and testing data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1], train_array[:, -1],
+                test_array[:, :-1], test_array[:, -1]
+            )
+
+            models = {
+                'LinearRegression': LinearRegression(),
+                'DecisionTreeRegressor': DecisionTreeRegressor(),
+                'RandomForestRegressor': RandomForestRegressor(),
+                'GradientBoostingRegressor': GradientBoostingRegressor(),
+                'AdaBoostRegressor': AdaBoostRegressor(),
+                'XGBRegressor': XGBRegressor(eval_metric='logloss'),
+                'CatBoostRegressor': CatBoostRegressor(verbose=0)
+            }
+
+            params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForestRegressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "XGBRegressor" : {
+                    "n_estimators": [100, 200, 300, 500],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.2],
+                    "max_depth": [3, 5, 7, 10],
+                    "booster": ["gbtree", "gblinear", "dart"],
+                    # "early_stopping_rounds": [10]
+                }
+                
+            }
+
+            model_report = evaluate_models(X_train, y_train, X_test, y_test, models, params=params)
+
+            # get the best model based on r2 score
+            best_model_score = max(model_report.values())
+
+            #get the best model name
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No Best Model found.")
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        # Raising the exception
+        except Exception as e:
+            raise CustomException(e, sys) from e
